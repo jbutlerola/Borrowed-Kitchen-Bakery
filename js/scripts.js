e1aeb3435e74eec8fc7de2bb5ee3b8a072919d88
Jack Butler <jackbutlerola@gmail.com>
2020-02-29 06:56:47 -0800

    fixed spacing issues, added more animations, added menu and footer.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,17 @@
 $(window).scroll(function(){
     if($(document).scrollTop() > ($('header').height())) {
-        $('nav').addClass("navRed");
+        $('nav').addClass("navAnimation");
+
     } else {
-        $('nav').removeClass("navRed");
+        $('nav').removeClass("navAnimation");
+    }
+});
+
+$(window).scroll(function(){
+    if($(document).scrollTop() > ($('header').height())) {
+        $('.nav-item').addClass("listAnimation");
+        
+    } else {
+        $('.nav-item').removeClass("listAnimation");
     }
 });
